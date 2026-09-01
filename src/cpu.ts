0fd45e4bd4a03d44baca8489bf5f81e76b749a0a
Sebastián Alvarez <seba.alvarez16@gmail.com>
2026-09-01 14:53:23 -0300

    Add A register

--- src/cpu.ts
+++ src/cpu.ts
@@ -3,10 +3,12 @@ const MEM_SIZE = 65536;
 export const CPU = () => {
   const memory = new Array(MEM_SIZE).fill(0);
   let PC = 0;
+  let rA = 0;
 
   const step = () => {
     const opcode = memory[PC];
     PC++;
+
     switch (opcode) {
       case 1:
         break;
@@ -19,6 +21,9 @@ export const CPU = () => {
   return {
     get PC() {
       return PC;
+    },
+    get A() {
+      return rA;
     },
     memory,
     step,
